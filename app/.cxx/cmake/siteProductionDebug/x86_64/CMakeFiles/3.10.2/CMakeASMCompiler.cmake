fe79217e13c0b0643904ec6eb3bf0fc7baeab1f1
Aleksandr <aleks.mykhailenko@gmail.com>
2020-03-11 07:09:46 +0200

    [DRD-335] add dynamic openvpn build

---
+++ app/.cxx/cmake/siteProductionDebug/x86_64/CMakeFiles/3.10.2/CMakeASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_COMPILER "/Users/aleksandrmykhailenko/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/Users/aleksandrmykhailenko/Library/Android/sdk/ndk-bundle/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/bin/x86_64-linux-android-ar")
+set(CMAKE_ASM_COMPILER_AR "CMAKE_ASM_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "/Users/aleksandrmykhailenko/Library/Android/sdk/ndk-bundle/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/bin/x86_64-linux-android-ranlib")
+set(CMAKE_ASM_COMPILER_RANLIB "CMAKE_ASM_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_LINKER "/Users/aleksandrmykhailenko/Library/Android/sdk/ndk-bundle/toolchains/x86_64-4.9/prebuilt/darwin-x86_64/bin/x86_64-linux-android-ld")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "Clang")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
