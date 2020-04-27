cede1d6619b3f7027df685595ae9520c439fffc6
Aleksandr <aleks.mykhailenko@gmail.com>
2020-04-27 09:50:42 +0300

    Redo sliding up panel

--- app/src/main/java/net/ivpn/client/v2/ConnectFragment.kt
+++ app/src/main/java/net/ivpn/client/v2/ConnectFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import net.ivpn.client.IVPNApplication
 import net.ivpn.client.R
 import net.ivpn.client.databinding.FragmentConnectBinding
@@ -16,6 +17,7 @@ import javax.inject.Inject
 class ConnectFragment : Fragment(), MultiHopViewModel.MultiHopNavigator {
 
     private lateinit var binding: FragmentConnectBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     @Inject
     lateinit var multihop: MultiHopViewModel
@@ -36,12 +38,13 @@ class ConnectFragment : Fragment(), MultiHopViewModel.MultiHopNavigator {
         super.onViewCreated(view, savedInstanceState)
         IVPNApplication.getApplication().appComponent.provideActivityComponent().create().inject(this)
         initViews()
-        view.post {
-            binding.slidingPanel.scrollView.initWith(view.height, binding.slidingPanel.exitServerLayout)
-        }
     }
 
     private fun initViews() {
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.slidingPanel.sheetLayout)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        bottomSheetBehavior.halfExpandedRatio = 0.000000001f
+
         multihop.navigator = this
         binding.slidingPanel.multihop = multihop
 
@@ -71,10 +74,15 @@ class ConnectFragment : Fragment(), MultiHopViewModel.MultiHopNavigator {
     }
 
     private fun enableMultiHop() {
-        binding.slidingPanel.scrollView.showView()
+        bottomSheetBehavior.setPeekHeight((resources.getDimension(R.dimen.slider_layout_single_hop_height)
+                + resources.getDimension(R.dimen.slider_layout_exit_layout_height)).toInt(), true)
+        binding.slidingPanel.exitServerLayout.visibility = View.VISIBLE
+        binding.slidingPanel.bottomSheet.requestLayout()
     }
 
     private fun disableMultiHop() {
-        binding.slidingPanel.scrollView.hideView()
+        bottomSheetBehavior.setPeekHeight((resources.getDimension(R.dimen.slider_layout_single_hop_height)).toInt(), true)
+        binding.slidingPanel.exitServerLayout.visibility = View.GONE
+        binding.slidingPanel.bottomSheet.requestLayout()
     }
 }
