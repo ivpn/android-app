5764a8e7b612707311cfefa9e71c97e293a61afa
Aleksandr <aleks.mykhailenko@gmail.com>
2020-05-02 11:26:52 +0300

    [DRD-345] add dark theme for connect screen

--- app/src/main/java/net/ivpn/client/v2/ConnectFragment.kt
+++ app/src/main/java/net/ivpn/client/v2/ConnectFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
 import net.ivpn.client.IVPNApplication
 import net.ivpn.client.R
 import net.ivpn.client.databinding.FragmentConnectBinding
@@ -40,10 +42,27 @@ class ConnectFragment : Fragment(), MultiHopViewModel.MultiHopNavigator {
         initViews()
     }
 
+    var nightMode = AppCompatDelegate.MODE_NIGHT_NO
     private fun initViews() {
-        bottomSheetBehavior = BottomSheetBehavior.from(binding.slidingPanel.sheetLayout)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//        AppCompatDelegate.setDefaultNightMode(nightMode)
+        binding.nightModeFab.setOnClickListener {
+            nightMode = if (nightMode ==  AppCompatDelegate.MODE_NIGHT_NO) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            AppCompatDelegate.setDefaultNightMode(nightMode)
+        }
+
+        bottomSheetBehavior = from(binding.slidingPanel.sheetLayout)
+        bottomSheetBehavior.state = STATE_COLLAPSED
         bottomSheetBehavior.halfExpandedRatio = 0.000000001f
+        bottomSheetBehavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == STATE_HIDDEN || newState == STATE_HALF_EXPANDED) {
+                    bottomSheetBehavior.state = STATE_EXPANDED
+                }
+            }
+        })
 
         multihop.navigator = this
         binding.slidingPanel.multihop = multihop
