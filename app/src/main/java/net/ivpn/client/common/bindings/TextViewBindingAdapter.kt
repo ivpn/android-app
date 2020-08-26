ce89cd02a860763e4a05489366a0ca087001930f
Aleksandr <aleks.mykhailenko@gmail.com>
2020-08-26 16:28:46 +0300

    [DRD-374] show toast message for multihop feature on the map screen

--- app/src/main/java/net/ivpn/client/common/bindings/TextViewBindingAdapter.kt
+++ app/src/main/java/net/ivpn/client/common/bindings/TextViewBindingAdapter.kt
@@ -1,9 +1,10 @@
 package net.ivpn.client.common.bindings
 
-import android.widget.RadioGroup
+import android.graphics.Typeface
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import net.ivpn.client.vpn.model.NetworkState
+
 
 @BindingAdapter("background")
 fun setBackground(view: TextView, backgroundId: Int) {
@@ -15,4 +16,9 @@ fun setText(view: TextView, state: NetworkState?) {
     state?.let {
         view.setText(it.textRes)
     }
+}
+
+@BindingAdapter("isBold")
+fun setBold(view: TextView, isBold: Boolean) {
+    view.setTypeface(null, if (isBold) Typeface.BOLD else Typeface.NORMAL)
 }
