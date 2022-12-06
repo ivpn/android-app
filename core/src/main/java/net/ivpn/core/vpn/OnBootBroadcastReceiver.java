bbc02af14aa65cde507fd749870212ab7cd7af47
Juraj Hilje <juraj.hilje@gmail.com>
2022-12-06 18:42:14 +0100

    fix: update Intent type in OnBootBroadcastReceiver.java

--- core/src/main/java/net/ivpn/core/vpn/OnBootBroadcastReceiver.java
+++ core/src/main/java/net/ivpn/core/vpn/OnBootBroadcastReceiver.java
@@ -47,7 +47,7 @@ public class OnBootBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (!intent.getAction().equals(Intent.BOOT_COMPLETED)) {
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
         }
         IVPNApplication.appComponent.provideActivityComponent().create().inject(this);
