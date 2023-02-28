482d5add303fcd24967ca46f3a80fbce8764d9e4
Juraj Hilje <juraj.hilje@gmail.com>
2023-02-28 18:03:28 +0100

    build: update OpenVPN to 0.7.33

--- core/src/main/java/de/blinkt/openvpn/core/NativeUtils.java
+++ core/src/main/java/de/blinkt/openvpn/core/NativeUtils.java
@@ -5,7 +5,11 @@
 
 package de.blinkt.openvpn.core;
 
+import android.os.Build;
+import java.security.InvalidKeyException;
+
 public class NativeUtils {
+    public static native byte[] rsasign(byte[] input, int pkey, boolean pkcs1padding) throws InvalidKeyException;
 
     public static native String[] getIfconfig() throws IllegalArgumentException;
 
@@ -17,7 +21,40 @@ public class NativeUtils {
 
     private static native String getJNIAPI();
 
-    static {
-        System.loadLibrary("opvpnutil");
+    public static native String getOpenVPN2GitVersion();
+
+    public static native String getOpenVPN3GitVersion();
+
+    static boolean osslutilloaded = false;
+
+    public static byte[] addRssPssPadding(int hashtype, int MSBits, int rsa_size, byte[] from)
+    {
+        loadOsslUtil();
+        return rsapss(hashtype, MSBits, rsa_size, from);
     }
+
+    private static void loadOsslUtil() {
+        if (!osslutilloaded) {
+            osslutilloaded = true;
+            System.loadLibrary("osslutil");
+        }
+    }
+
+    private static native byte[] rsapss(int hashtype, int MSBits, int rsa_size, byte[] from);
+
+    public final static int[] openSSLlengths = {
+        16, 64, 256, 1024, 1500, 8 * 1024, 16 * 1024
+    };
+
+    public static native double[] getOpenSSLSpeed(String algorithm, int testnum);
+
+    static {
+        if (!isRoboUnitTest()) {
+            System.loadLibrary("ovpnutil");
+        }
+    }
+
+    public static boolean isRoboUnitTest() {
+        return "robolectric".equals(Build.FINGERPRINT); }
+
 }
