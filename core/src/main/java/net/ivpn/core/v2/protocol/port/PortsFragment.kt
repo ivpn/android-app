4108c587c3ca97140652c06cd87fd8a14b12fefc
Juraj Hilje <juraj.hilje@gmail.com>
2023-05-13 19:13:56 +0200

    feat(settings): create PortsScreen.kt

--- core/src/main/java/net/ivpn/core/v2/protocol/port/PortsFragment.kt
+++ core/src/main/java/net/ivpn/core/v2/protocol/port/PortsFragment.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.fragment.app.Fragment
-import androidx.compose.runtime.Composable
-import androidx.compose.material.*
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.unit.dp
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -45,34 +39,6 @@ class PortsFragment : Fragment() {
         val navController = findNavController()
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-    }
-
-}
-
-data class ProtocolPort(
-    val proto: String,
-    val port: Int
-)
-
-@Composable
-fun PortsScreen() {
-
-    val portsList = listOf(
-        ProtocolPort("UDP", 2001),
-        ProtocolPort("UDP", 2002),
-        ProtocolPort("UDP", 2003),
-        ProtocolPort("UDP", 2004),
-        ProtocolPort("TCP" , 2005),
-        ProtocolPort("TCP", 2006)
-    )
-
-    LazyColumn(
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(portsList) { item ->
-            Text(text = "${item.proto} ${item.port}")
-            Divider()
-        }
     }
 
 }
---
+++ core/src/main/java/net/ivpn/core/v2/protocol/port/PortsScreen.kt
@@ -0,0 +1,52 @@
+package net.ivpn.core.v2.protocol.port
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ProtocolPort(
+    val proto: String,
+    val port: Int
+)
+
+@Composable
+fun PortsScreen() {
+    val portsList = listOf(
+        ProtocolPort("UDP", 2001),
+        ProtocolPort("UDP", 2002),
+        ProtocolPort("UDP", 2003),
+        ProtocolPort("UDP", 2004),
+        ProtocolPort("TCP" , 2005),
+        ProtocolPort("TCP", 2006)
+    )
+
+    LazyColumn {
+        items(portsList) {
+            PortListItem(port = it)
+        }
+    }
+}
+
+@Composable
+fun PortListItem(port: ProtocolPort) {
+    Row {
+        Column(
+            modifier = Modifier
+                .padding(18.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(text = "${port.proto} ${port.port}")
+        }
+    }
+    Divider()
+}
