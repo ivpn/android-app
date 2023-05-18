5108e2d23ed6179bf79492769ff0204f5150abac
Juraj Hilje <juraj.hilje@gmail.com>
2023-05-18 20:51:38 +0200

    refactor: clean up PortsScreen.kt

--- core/src/main/java/net/ivpn/core/v2/protocol/port/PortsScreen.kt
+++ core/src/main/java/net/ivpn/core/v2/protocol/port/PortsScreen.kt
@@ -1,11 +1,9 @@
 package net.ivpn.core.v2.protocol.port
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
