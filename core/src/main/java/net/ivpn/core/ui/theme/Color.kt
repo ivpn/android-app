b41091518716b5c366a5f140f7e95ae201f5a029
Juraj Hilje <juraj.hilje@gmail.com>
2023-05-19 17:07:07 +0200

    feat(settings): update CustomPortScreen.kt

--- core/src/main/java/net/ivpn/core/ui/theme/Color.kt
+++ core/src/main/java/net/ivpn/core/ui/theme/Color.kt
@@ -8,7 +8,10 @@ val colorPrimary = Color(0xFF398FE6)
 
 @Immutable
 data class CustomColors(
-    val textFieldLabel: Color = Color.Unspecified
+    val textFieldLabel: Color = Color.Unspecified,
+    val textFieldBackground: Color = Color.Unspecified,
+    val textFieldText: Color = Color.Unspecified,
+    val textFieldPlaceholder: Color = Color.Unspecified
 )
 
 val LocalColors = staticCompositionLocalOf {
@@ -16,9 +19,15 @@ val LocalColors = staticCompositionLocalOf {
 }
 
 val OnLightCustomColors = CustomColors(
-    textFieldLabel = Color(0xFF2A394B)
+    textFieldLabel = Color(0xFF2A394B),
+    textFieldBackground = Color(0x54D3DFE6),
+    textFieldText = Color(0xFF2A394B),
+    textFieldPlaceholder = Color(0x802A394B)
 )
 
 val OnDarkCustomColors = CustomColors(
-    textFieldLabel = Color(0xFFFFFFFF)
+    textFieldLabel = Color(0xFFFFFFFF),
+    textFieldBackground = Color(0xFF1C1C1E),
+    textFieldText = Color(0xFFFFFFFF),
+    textFieldPlaceholder = Color(0x80FFFFFF)
 )
--- core/src/main/java/net/ivpn/core/v2/protocol/port/CustomPortScreen.kt
+++ core/src/main/java/net/ivpn/core/v2/protocol/port/CustomPortScreen.kt
@@ -7,14 +7,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,12 +43,17 @@ fun PortInput() {
             .fillMaxWidth()
     ) {
         TextFieldLabel("Port")
-        TextField(
+        OutlinedTextField(
             value = "",
-            onValueChange = {},
+            onValueChange = { it },
             placeholder = { Text("5500 - 19999, 30000 - 65000") },
-            modifier = Modifier
-                .fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = LocalColors.current.textFieldBackground,
+                unfocusedIndicatorColor = Color.Transparent,
+                placeholderColor = LocalColors.current.textFieldPlaceholder,
+                textColor = LocalColors.current.textFieldText
+            )
         )
     }
 }
