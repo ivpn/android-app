e4d8127e74757dfc94aad027b77088f75add2670
Juraj Hilje <juraj.hilje@gmail.com>
2023-07-07 15:17:13 +0200

    feat(antitracker): create model/AntiTrackerDns.kt

---
+++ core/src/main/java/net/ivpn/core/rest/data/model/AntiTrackerDns.kt
@@ -0,0 +1,84 @@
+package net.ivpn.core.rest.data.model
+
+/*
+ IVPN Android app
+ https://github.com/ivpn/android-app
+
+ Created by Juraj Hilje.
+ Copyright (c) 2023 Privatus Limited.
+
+ This file is part of the IVPN Android app.
+
+ The IVPN Android app is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ The IVPN Android app is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License
+ along with the IVPN Android app. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import com.google.gson.Gson
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class AntiTrackerDns {
+
+    @SerializedName("Name")
+    @Expose
+    var name: String = ""
+
+    @SerializedName("Description")
+    @Expose
+    var description: String = ""
+
+    @SerializedName("Normal")
+    @Expose
+    var normal: String = ""
+
+    @SerializedName("Hardcore")
+    @Expose
+    var hardcore: String = ""
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is AntiTrackerDns) return false
+        return name == other.name && normal == other.normal
+    }
+
+    fun toJson(): String {
+        return Gson().toJson(this)
+    }
+
+    fun toThumbnail(): String {
+        return description
+    }
+
+    companion object {
+
+        fun from(json: String): AntiTrackerDns {
+            return Gson().fromJson(json, AntiTrackerDns::class.java)
+        }
+
+    }
+
+}
+
+class DnsServers {
+
+    @SerializedName("DnsServers")
+    @Expose
+    lateinit var list: List<AntiTrackerDns>
+
+}
+
+class AntiTrackerPlus {
+
+    @SerializedName("antitracker_plus")
+    @Expose
+    lateinit var dnsServers: DnsServers
+
+}
