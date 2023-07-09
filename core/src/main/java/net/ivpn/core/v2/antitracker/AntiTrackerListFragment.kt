5a21a1c947685ebbcd50312d4c8fdbe7b47ea6a8
Juraj Hilje <juraj.hilje@gmail.com>
2023-07-09 16:37:49 +0200

    feat(antitracker): create AntiTrackerListFragment.kt

---
+++ core/src/main/java/net/ivpn/core/v2/antitracker/AntiTrackerListFragment.kt
@@ -0,0 +1,77 @@
+package net.ivpn.core.v2.antitracker
+
+/*
+ IVPN Android app
+ https://github.com/ivpn/android-app
+
+ Created by Juraj Hilje.
+ Copyright (c) 2023 Privatus Limited.
+
+ This file is part of the IVPN Android app.
+
+ The IVPN Android app is free software: you can redistribute it and/or
+ modify it under the terms of the GNU General Public License as published by the Free
+ Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ The IVPN Android app is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details.
+
+ You should have received a copy of the GNU General Public License
+ along with the IVPN Android app. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import net.ivpn.core.IVPNApplication
+import net.ivpn.core.R
+import net.ivpn.core.common.dagger.ApplicationScope
+import net.ivpn.core.databinding.FragmentAntiTrackerListBinding
+import net.ivpn.core.ui.theme.AppTheme
+
+@ApplicationScope
+class AntiTrackerListFragment : Fragment() {
+
+    lateinit var binding: FragmentAntiTrackerListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        IVPNApplication.appComponent.provideActivityComponent().create().inject(this)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_anti_tracker_list, container, false)
+        return binding.root.apply {
+            findViewById<ComposeView>(R.id.view_ports).setContent {
+                AppTheme {
+
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+}
