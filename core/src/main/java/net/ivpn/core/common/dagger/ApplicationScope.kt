f84d80a07e76eca728a39d1187d75c4228a70406
Juraj Hilje <juraj.hilje@gmail.com>
2023-07-27 12:15:20 +0200

    refactor: fix deprecations

--- core/src/main/java/net/ivpn/core/common/dagger/ApplicationScope.kt
+++ core/src/main/java/net/ivpn/core/common/dagger/ApplicationScope.kt
@@ -1,7 +1,5 @@
 package net.ivpn.core.common.dagger
 
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 import javax.inject.Scope
 
 /*
@@ -27,5 +25,5 @@ import javax.inject.Scope
 */
 
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ApplicationScope 
