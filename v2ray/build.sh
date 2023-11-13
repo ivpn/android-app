63a225cd8ba10f0996e965a63f322293ea6ae7ef
Juraj Hilje <juraj.hilje@gmail.com>
2023-11-13 16:45:43 +0100

    ci: update build.yml and codeql.yml

--- v2ray/build.sh
+++ v2ray/build.sh
@@ -16,5 +16,5 @@ go get golang.org/x/mobile/cmd/gomobile
 echo "=> Build Android library.."
 OUT_AAR=../libs/V2RayControl.aar
 gomobile init &&
-gomobile bind -trimpath -ldflags "-s -w" -target=android -o ${OUT_AAR}
-echo "=> Android build completed (out: ${OUT_AAR})"
+gomobile bind -trimpath -ldflags "-s -w" -target=android -androidapi 21 -o ${OUT_AAR}
+# echo "=> Android build completed (out: ${OUT_AAR})"
